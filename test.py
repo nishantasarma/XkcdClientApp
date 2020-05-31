96e2e2f8847c1a00969dfbdecdd55fd2b4f5da73
nishantasarma <nishantasarma@gmail.com>
2020-05-31 12:18:32 +0530

    Corrected Variable name

--- test.py
+++ test.py
@@ -9,7 +9,17 @@ if __name__ == '__main__':
     unix_args = 'hn:os'
     gnu_args = ['help','comicnum=','print','save-image']
 
-    oplist, args = getopt.getopt(cmd_line_args,unix_args,long_args)
+    oplist, args = getopt.getopt(cmd_line_args,unix_args,gnu_args)
 
     print(args) #Extra arguments that are not part of the uni_args or gnu_args
     print(oplist) #oplist is a list of tuples
+
+    for opt, arg in oplist:
+        if opt == '-h' or '--help':
+            print('help message')
+        elif opt == '-n' or '--comicnum':
+            print('Get the comic number ' + str(arg))
+        elif opt == '-o' or --'print':
+            print('print output in format json/text')
+        elif opt == '-s' or '--save-image':
+            print('Download image in this directory')
